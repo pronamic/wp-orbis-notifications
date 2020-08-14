5629e7596f04a5601f39f32d7a0cbb756e683fb5
Reüel van der Steege <reuel@pronamic.nl>
2020-08-14 16:02:49 +0200

    Move subscription email messages to section.

--- includes/template.php
+++ includes/template.php
@@ -27,12 +27,27 @@ function orbis_company_sections_email_messages( $sections ) {
 add_filter( 'orbis_company_sections', 'orbis_company_sections_email_messages', 30 );
 
 /**
- * Orbis subscription email messages.
+ * Orbis subscription section email messages.
  *
- * @return void
+ * @param array $sections Sections.
+ * @return array
  */
-function orbis_notifications_subscription_email_messages() {
-	require __DIR__ . '/../templates/subscription-email-messages.php';
+function orbis_subscription_section_email_messages( $sections ) {
+	$sections[] = array(
+		'id'       => 'email-messages',
+		'name'     => __( 'Email messages', 'orbis-notifications' ),
+		'callback' => function() {
+			if ( ! is_singular( 'orbis_subscription' ) ) {
+				return;
+			}
+
+			global $orbis_notifications_plugin;
+
+			$orbis_notifications_plugin->plugin_include( 'templates/subscription-email-messages.php' );
+		},
+	);
+
+	return $sections;
 }
 
-add_filter( 'orbis_after_main_content', 'orbis_notifications_subscription_email_messages' );
+add_filter( 'orbis_subscription_sections', 'orbis_subscription_section_email_messages', 30 );
--- templates/subscription-email-messages.php
+++ templates/subscription-email-messages.php
@@ -41,48 +41,40 @@ $tz  = \wp_timezone();
 
 if ( $email_messages ) : ?>
 
-<div class="card mb-3">
-	<div class="card-header">
-		<?php esc_html_e('Email messages', 'orbis-notifications' ); ?>
-	</div>
+	<table class="table table-striped mb-0">
+		<thead>
+			<tr>
+				<th scope="col"><?php \esc_html_e( 'Created', 'orbis-notifications' ); ?></th>
+				<th scope="col"><?php \esc_html_e( 'To', 'orbis-notifications' ); ?></th>
+				<th scope="col"><?php \esc_html_e( 'Subject', 'orbis-notifications' ); ?></th>
+			</tr>
+		</thead>
 
-	<div class="table-responsive" id="orbis-subscription-email-messages">
-		<table class="table table-striped mb-0">
-			<thead>
+		<tbody>
+
+			<?php foreach ( $email_messages as $email_message ) : ?>
+
 				<tr>
-					<th scope="col"><?php \esc_html_e( 'Created', 'orbis-notifications' ); ?></th>
-					<th scope="col"><?php \esc_html_e( 'To', 'orbis-notifications' ); ?></th>
-					<th scope="col"><?php \esc_html_e( 'Subject', 'orbis-notifications' ); ?></th>
+					<td><?php echo \esc_html( ( new \DateTimeImmutable( $email_message->created_at, $utc ) )->setTimezone( $tz )->format( 'd-m-Y H:i' ) ); ?></td>
+					<td><?php echo \esc_html( $email_message->to_email ); ?></td>
+					<td>
+						<?php
+
+						$url = \home_url( \user_trailingslashit( 'email-messages/' . $email_message->id ) );
+
+						\printf(
+							'<a href="%s">%s</a>',
+							\esc_url( $url ),
+							\esc_html( $email_message->subject )
+						);
+
+						?>
+					</td>
 				</tr>
-			</thead>
 
-			<tbody>
+			<?php endforeach; ?>
 
-				<?php foreach ( $email_messages as $email_message ) : ?>
-
-					<tr>
-						<td><?php echo \esc_html( ( new \DateTimeImmutable( $email_message->created_at, $utc ) )->setTimezone( $tz )->format( 'd-m-Y H:i' ) ); ?></td>
-						<td><?php echo \esc_html( $email_message->to_email ); ?></td>
-						<td>
-							<?php
-
-							$url = \home_url( \user_trailingslashit( 'email-messages/' . $email_message->id ) );
-
-							\printf(
-								'<a href="%s">%s</a>',
-								\esc_url( $url ),
-								\esc_html( $email_message->subject )
-							);
-
-							?>
-						</td>
-					</tr>
-
-				<?php endforeach; ?>
-
-			</tbody>
-		</table>
-	</div>
-</div>
+		</tbody>
+	</table>
 
 <?php endif; ?>
